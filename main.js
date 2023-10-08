0fac1d32c112b023ba3ec6051797551276265417
mrkm090 <55298394+mrkm090@users.noreply.github.com>
2023-10-08 17:48:43 +0200

    Modify DOM with js.

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ console.log('Good Day');
 const first_name = 'Marek';
 const age = 39;
 
-console.log(`My name is ${first_name}, and I am ${age} year's old.`)
+console.log(`My name is ${first_name}, and I am ${age} year's old.`);
+
+const header = document.querySelector(".page-header__heading-js");
+
+header.innerHTML = 'To jest bardzo fajoska strona!!! ;)';
+header.style.color = 'red';
+
+const empty = document.querySelector(".empty__header-js");
+empty.innerHTML = 'To jest treść dodana w JS'
+empty.style.color = 'red';
+
+console.log(header.innerHTML);
